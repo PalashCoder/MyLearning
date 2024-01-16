03486ed9396703116cab12f6108c625fc0ac12cc
Palash <ng.palash.das@gmail.com>
2024-01-16 22:40:00 +0530

    today

---
+++ DSA/C++/patterns/1.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    for (int i = 0; i <= 5; i++)
+    {
+        for (int i = 0; i <= 5; i++)
+        {
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+    
+    return 0;
+}
