1b2ea59cfe7a25043a38ba26485566d75850ec91
Palash <ng.palash.das@gmail.com>
2024-06-03 21:22:34 +0530

    starting c#

---
+++ C#/Solution1/hello.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace helloworlld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Writeline("Hello World");
+        }
+    }
+}
